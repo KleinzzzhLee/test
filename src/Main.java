dd213d4fe11d89adde1cf114b69130f8026d553c
西安-李自豪 <zzzhlee0930@163.com>
2024-12-06 16:04:31 +0800

    feat(scope): 遴选描述

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("zxcvasdfasdfsd");
+    }
+
+    private void test2() {
+        System.out.println("test2");
+    }
+
+    private void test3() {
+        System.out.println("test3");
+    }
+
+    private void test4() {
+        System.out.println("test4");
+    }
+
+    private void lingxuan() {
+        System.out.println("遴选");
     }
 }
